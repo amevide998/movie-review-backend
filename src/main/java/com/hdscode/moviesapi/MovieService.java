b3c1c277472816ecc9b4340eccf9176192eaec84
amevide998 <hdsvidi@gmail.com>
2023-10-17 00:18:40 +0700

    initial movie service

---
+++ src/main/java/com/hdscode/moviesapi/MovieService.java
@@ -0,0 +1,20 @@
+package com.hdscode.moviesapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+
+}
